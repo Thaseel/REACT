eecfef8c2d8222cb98cbc1cabae06dfc533535f3
Thaseel <thaseelahmed555@gmail.com>
2025-08-13 11:24:54 +0530

    Adding more components

--- FRONTEND/src/user/components/UsersList.js
+++ FRONTEND/src/user/components/UsersList.js
@@ -1,17 +1,20 @@
 import React from "react";
 
 import UserItem from "./UserItem";
+import Card from '../../shared/components/UIElements/Card';
 import './UsersList.css';
 
 const UsersList = props => {
     if (props.items.length === 0) {
         return (<div className="center">
-            <h2>No Users Found.</h2>
+            <card>
+                <h2>No Users Found.</h2>
+            </card>
         </div>
         );
     }
 
-    return <ul>
+    return <ul className="users-list">
         {props.items.map(user => {
             return <UserItem key={user.id} id={user.id} image={user.image} name={user.name} placeCount={user.places} />
         })}
--- FRONTEND/src/user/pages/Users.js
+++ FRONTEND/src/user/pages/Users.js
@@ -3,7 +3,7 @@ import React from "react";
 import UsersList from '../components/UsersList';
 
 const Users = () => {
-    const USERS = [{id:'u1', name: 'LadiesWasheroom', image:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Fcreative-arts&psig=AOvVaw2Ddtw5-EzOyL_UKPQIcWGH&ust=1755095739354000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLjJ9K6_hY8DFQAAAAAdAAAAABAE', places : 3}]
+    const USERS = [{id:'u1', name: 'LadiesWasheroom', image:'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', places : 3}]
 
     return <UsersList items={USERS} />
 }
