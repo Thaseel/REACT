9c98f6d18e21db90d4ecdd94e5f337fdf57b1db7
Thaseel R <thaseel.r@sap.com>
2025-08-14 13:01:24 +0000

    understanding portals

--- FRONTEND/src/shared/components/Navigation/SideDrawer.js
+++ FRONTEND/src/shared/components/Navigation/SideDrawer.js
@@ -1,9 +1,12 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 
 import './SideDrawer.css';
 
 const SideDrawer = props => {
-    return <aside className="side-drawer">{props.children}</aside>
+    const content =  <aside className="side-drawer">{props.children}</aside>;
+
+    return ReactDOM.createPortal(content, document.getElementById('drawer-hook')); {/* ReactDOM.createPortal helps you to put the JSX elements where ever you wanna place inside the index.html file */}
 };
 
 export default SideDrawer;
