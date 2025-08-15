d635244f1d49a31146c64645ac93722d7a74826f
Thaseel <thaseelahmed555@gmail.com>
2025-08-15 17:52:09 +0530

    Fix for Side Drawer

--- FRONTEND/src/shared/components/Navigation/MainNavigation.js
+++ FRONTEND/src/shared/components/Navigation/MainNavigation.js
@@ -1,45 +1,48 @@
-import React, {useState} from "react";
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import MainHeader from "./MainHeader";
+import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
-import SideDrawer from "./SideDrawer";
-import Backdrop from "../UIElements/Backdrop";
+import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop';
 import './MainNavigation.css';
 
-const MainNavigation = props =>{
-    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+const MainNavigation = props => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
-    const openDrawer = () => {
-        setDrawerIsOpen(true);
-    };
+  const openDrawer = () => {
+    setDrawerIsOpen(true);
+  };
 
-    const closeDrawer = () => {
-        setDrawerIsOpen(false);
-    }
-    return(
-        <React.Fragment>  {/* React.Fragment helps you return more than one JSX element */}
-            {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
-            {drawerIsOpen && <SideDrawer>
-                <nav className="main-navigation__drawer-nav">
-                    <NavLinks />
-                </nav>
-            </SideDrawer>}
-            <MainHeader>
-                <button className="main-navigation__menu-btn" onClick={openDrawer}>
-                    <span />
-                    <span />
-                    <span />
-                </button>
-                <h1 className="main-navigation__title">
-                    <Link to="/">Your Places</Link>
-                </h1>
-                <nav className="main-navigation__header-nav">
-                    <NavLinks />
-                </nav>
-            </MainHeader>
-        </React.Fragment>
-    );
-}
+  const closeDrawer = () => {
+    setDrawerIsOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
+      {drawerIsOpen && (
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      )}
+      <MainHeader>
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">YourPlaces</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
+  );
+};
 
 export default MainNavigation;
