99b5cf8e11b4c12c9585c71059347f2ac33a92f0
Thaseel <thaseelahmed555@gmail.com>
2025-08-18 12:10:29 +0530

    Managing State in a input component - 61

--- FRONTEND/src/shared/components/FormElements/Input.js
+++ FRONTEND/src/shared/components/FormElements/Input.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useReducer } from "react";
 
 import './Input.css';
 
-const Input = props => {
-    const element = props.element === 'input' ? <input id={props.id} type={props.type} placeholder={props.placeholder} /> : <textarea id={props.id} rows={props.rows || 3} />
+const inputReducer = (state, action) => {
+    {/* ...state is used for making a copy of existing state so that we don't lose it later */}
+    switch(action.type) {
+        case 'CHANGE' : 
+            return {
+                ...state,
+                value: action.val,
+                isValid : true
+            };
+        default: return state
+    }
+}
 
-    return <div className={`form-control`}>
+const Input = props => {
+    {/* useReducer can be used instead of useState when you have more complex state or interconnected state basically advanced version of useState */}
+    const [inputState, dispatch] = useReducer(inputReducer, {value : '', isValid: false});
+
+    const changeHandler = event => {
+        dispatch({type : 'CHANGE', val : event.target.value})
+    };
+
+    const element = props.element === 'input' ? <input id={props.id} type={props.type} placeholder={props.placeholder} onChange={changeHandler} value={inputState.value}/> : <textarea id={props.id} rows={props.rows || 3} onChange={changeHandler} value={inputState.value} />
+
+    return <div className={`form-control ${!inputState.isVaild && 'form-control--invalid'}`}>
         <label htmlFor={props.id}>{props.label}</label>
         {element}
+        {!inputState.isValid && <p>{props.errorText}</p>}
     </div>
 };
 
