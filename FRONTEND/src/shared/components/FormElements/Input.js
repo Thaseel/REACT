fed2890b2dc9ab8853d2a0f842cf4d2a875aee4d
Thaseel <thaseelahmed555@gmail.com>
2025-08-19 12:11:11 +0530

    Managin Form-wide state - 65

--- FRONTEND/src/shared/components/FormElements/Input.js
+++ FRONTEND/src/shared/components/FormElements/Input.js
@@ -28,11 +28,11 @@ const Input = props => {
     const [inputState, dispatch] = useReducer(inputReducer, {value : '', isValid: false, isTouched : false});
 
     const { id, onInput } = props;
-    const { value, isVaild } = inputState;
+    const { value, isValid } = inputState;
 
     useEffect(() => {
-        onInput(id, value, isVaild)
-    }, [id, value, isVaild, onInput])
+        onInput(id, value, isValid)
+    }, [id, value, isValid, onInput])
 
     const changeHandler = event => {
         dispatch({type : 'CHANGE', val : event.target.value, validators : props.validators})
@@ -46,7 +46,7 @@ const Input = props => {
 
     const element = props.element === 'input' ? <input id={props.id} type={props.type} placeholder={props.placeholder} onChange={changeHandler} onBlur={touchHandler} value={inputState.value}/> : <textarea id={props.id} rows={props.rows || 3} onChange={changeHandler} onBlur={touchHandler} value={inputState.value} />
 
-    return <div className={`form-control ${!inputState.isVaild && inputState.isTouched && 'form-control--invalid'}`}>
+    return <div className={`form-control ${!inputState.isValid && inputState.isTouched && 'form-control--invalid'}`}>
         <label htmlFor={props.id}>{props.label}</label>
         {element}
         {!inputState.isValid && inputState.isTouched && <p>{props.errorText}</p>}
