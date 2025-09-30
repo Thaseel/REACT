005f7b67bc103845d8459cb0c7f5e2c587fbe02c
Thaseel <thaseelahmed555@gmail.com>
2025-09-30 14:41:14 +0530

    Adding Controllers - 97

---
+++ BACKEND/controllers/places-controller.js
@@ -0,0 +1,64 @@
+const HttpError = require('../models/http-error');
+
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'One of the most beautiful building in the world!',
+        imageUrl : 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+        address: '20 W 34th St., New York, NY 10001',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9882393
+        },
+        creator: 'u1'
+    },
+    {
+        id : 'p2',
+        title : 'Emp. State Building',
+        description : 'One of the most beautiful building in the world!',
+        imageUrl : 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+        address : '20 W 34th St., New York, NY 10001',
+        location : {
+            lat : 40.7484405,
+            lng : -73.9882393
+        },
+        creator : 'u2'
+    },
+
+]
+
+const getPlaceById = (req, res, next) => {
+    const placeId = req.params.pid          // {pid: p1}
+
+    const place = DUMMY_PLACES.find(p => {
+        return p.id === placeId
+    })
+
+    if(!place){
+        throw new HttpError('Could not find a place for the provided Id.', 404);            // throw is used when we have only synchronous operations but if we have asynchronous operations we use 'next'
+    }
+
+    res.json({ place });                  // {place} --> {place : place}
+}
+
+//Alternatives to create functions
+// function getPlaceById() {...}
+//const getPlaceById = function() {...}
+
+const getPlaceByUserId = (req, res, next) => {
+    const userId = req.params.uid         
+
+    const place = DUMMY_PLACES.find(p => {
+        return p.creator === userId
+    })
+
+    if(!place){
+        return next(new HttpError('Could not find a place for the provided user Id.', 404));
+    }
+
+    res.json({ place });                  // {place} --> {place : place}
+}
+
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUserId = getPlaceByUserId;
--- BACKEND/routes/places-routes.js
+++ BACKEND/routes/places-routes.js
@@ -1,63 +1,11 @@
 const express = require('express');
 
-const HttpError = require('../models/http-error');
+const placeControllers = require('../controllers/places-controller');
 
 const router = express.Router();
 
-const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most beautiful building in the world!',
-        imageUrl : 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
-        address: '20 W 34th St., New York, NY 10001',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9882393
-        },
-        creator: 'u1'
-    },
-    {
-        id : 'p2',
-        title : 'Emp. State Building',
-        description : 'One of the most beautiful building in the world!',
-        imageUrl : 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
-        address : '20 W 34th St., New York, NY 10001',
-        location : {
-            lat : 40.7484405,
-            lng : -73.9882393
-        },
-        creator : 'u2'
-    },
+router.get('/:pid', placeControllers.getPlaceById);
 
-]
-
-router.get('/:pid', (req, res, next) => {
-    const placeId = req.params.pid          // {pid: p1}
-
-    const place = DUMMY_PLACES.find(p => {
-        return p.id === placeId
-    })
-
-    if(!place){
-        throw new HttpError('Could not find a place for the provided Id.', 404);            // throw is used when we have only synchronous operations but if we have asynchronous operations we use 'next'
-    }
-
-    res.json({ place });                  // {place} --> {place : place}
-});
-
-router.get('/user/:uid', (req, res, next) => {
-    const userId = req.params.uid         
-
-    const place = DUMMY_PLACES.find(p => {
-        return p.creator === userId
-    })
-
-    if(!place){
-        return next(new HttpError('Could not find a place for the provided user Id.', 404));
-    }
-
-    res.json({ place });                  // {place} --> {place : place}
-});
+router.get('/user/:uid', placeControllers.getPlaceByUserId);
 
 module.exports = router;
