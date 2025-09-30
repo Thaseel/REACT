5e305503921fe6522db8ed5f859d04fd7d5649be
Thaseel <thaseelahmed555@gmail.com>
2025-09-30 13:15:05 +0530

    Adding our own Error model - 96

--- BACKEND/routes/places-routes.js
+++ BACKEND/routes/places-routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const HttpError = require('../models/http-error');
+
 const router = express.Router();
 
 const DUMMY_PLACES = [
@@ -38,9 +40,7 @@ router.get('/:pid', (req, res, next) => {
     })
 
     if(!place){
-        const error = new Error('Could not find a place for the provided Id.');
-        error.code = 404;
-        throw error;            // throw is used when we have only synchronous operations but if we have asynchronous operations we use 'next'
+        throw new HttpError('Could not find a place for the provided Id.', 404);            // throw is used when we have only synchronous operations but if we have asynchronous operations we use 'next'
     }
 
     res.json({ place });                  // {place} --> {place : place}
@@ -54,9 +54,7 @@ router.get('/user/:uid', (req, res, next) => {
     })
 
     if(!place){
-        const error = new Error('Could not find a place for the provided user Id.');
-        error.code = 404;
-        return next(error);
+        return next(new HttpError('Could not find a place for the provided user Id.', 404));
     }
 
     res.json({ place });                  // {place} --> {place : place}
