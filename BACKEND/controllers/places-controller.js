311d6b802766ddbca13c014bdcd1a1a1d8d394c7
Thaseel <thaseelahmed555@gmail.com>
2025-09-30 22:20:09 +0530

    Using Google's geocoding API to convert an Address into Coordinates - 106

--- BACKEND/controllers/places-controller.js
+++ BACKEND/controllers/places-controller.js
@@ -1,116 +1,112 @@
-const {v4: uuidv4} = require('uuid');
-const {validationResult} = require('express-validator');
+const {v4 : uuidv4} = require('uuid');
+const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
+const getCoordsForAddress = require('../util/location');
 
 let DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most beautiful building in the world!',
-        imageUrl : 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
-        address: '20 W 34th St., New York, NY 10001',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9882393
-        },
-        creator: 'u1'
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516
     },
-    {
-        id : 'p2',
-        title : 'Emp. State Building',
-        description : 'One of the most beautiful building in the world!',
-        imageUrl : 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
-        address : '20 W 34th St., New York, NY 10001',
-        location : {
-            lat : 40.7484405,
-            lng : -73.9882393
-        },
-        creator : 'u1'
-    },
-
-]
+    address: '20 W 34th St, New York, NY 10001',
+    creator: 'u1'
+  }
+];
 
 const getPlaceById = (req, res, next) => {
-    const placeId = req.params.pid          // {pid: p1}
+  const placeId = req.params.pid; // { pid: 'p1' }
 
-    const place = DUMMY_PLACES.find(p => {
-        return p.id === placeId
-    })
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeId;
+  });
 
-    if(!place){
-        throw new HttpError('Could not find a place for the provided Id.', 404);            // throw is used when we have only synchronous operations but if we have asynchronous operations we use 'next'
-    }
+  if (!place) {
+    throw new HttpError('Could not find a place for the provided id.', 404);
+  }
 
-    res.json({ place });                  // {place} --> {place : place}
-}
+  res.json({ place }); // => { place } => { place: place }
+};
 
-//Alternatives to create functions
-// function getPlaceById() {...}
-//const getPlaceById = function() {...}
+// function getPlaceById() { ... }
+// const getPlaceById = function() { ... }
 
 const getPlacesByUserId = (req, res, next) => {
-    const userId = req.params.uid         
+  const userId = req.params.uid;
 
-    const places = DUMMY_PLACES.filter(p => {
-        return p.creator === userId
-    })
+  const places = DUMMY_PLACES.filter(p => {
+    return p.creator === userId;
+  });
 
-    if(!places || places.length === 0){
-        return next(new HttpError('Could not find places for the provided user Id.', 404));
-    }
+  if (!places || places.length === 0) {
+    return next(
+      new HttpError('Could not find places for the provided user id.', 404)
+    );
+  }
 
-    res.json({ places });                  // {place} --> {place : place}
-}
+  res.json({ places });
+};
 
-const createPlace = (req, res, next) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        throw new HttpError('Invalid inputs passed, please check your data.', 422);
-    }
+const createPlace = async (req, res, next) => {
+  const errors = validationResult(req);
+//   if (!errors.isEmpty()) {
+//     return next(new HttpError('Invalid inputs passed, please check your data.', 422));
+//   }
 
-    const {title, description, coordinates, address, creator} = req.body;
-    // const title = req.body.title --> we can use instead of the above line but object destructuring is more easier to use and is modern
-    const createdPlace = {
-        id : uuidv4(),
-        title,
-        description,
-        location : coordinates,
-        address,
-        creator
-    };
+  const { title, description, address, creator } = req.body;
 
-    DUMMY_PLACES.push(createdPlace);
+  let coordinates;
+  try {
+    coordinates = await getCoordsForAddress(address);
+  } catch (error) {
+    return next(error);
+  }
 
-    res.status(201).json({place : createdPlace})
-}
+  // const title = req.body.title;
+  const createdPlace = {
+    id: uuidv4(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator
+  };
+
+  DUMMY_PLACES.push(createdPlace); //unshift(createdPlace)
+
+  res.status(201).json({ place: createdPlace });
+};
 
 const updatePlace = (req, res, next) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        throw new HttpError('Invalid inputs passed, please check your data.', 422);
-    }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    throw new HttpError('Invalid inputs passed, please check your data.', 422);
+  }
 
-    const {title, description} = req.body;
-    const placeId = req.params.pid;
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
 
-    const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
-    const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
-    updatedPlace.title = title;
-    updatedPlace.description = description;
+  const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
+  const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
+  updatedPlace.title = title;
+  updatedPlace.description = description;
 
-    DUMMY_PLACES[placeIndex] = updatedPlace;
+  DUMMY_PLACES[placeIndex] = updatedPlace;
 
-    res.status(200).json({place : updatedPlace});
+  res.status(200).json({ place: updatedPlace });
 };
 
 const deletePlace = (req, res, next) => {
-    const placeId = req.params.pid;
-    if(!DUMMY_PLACES.find(p => p.id === placeId)){
-        throw new HttpError('Could not find the place you are trying to delete.', 404)
-    }
-    DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId)
-    res.status(200).json({message : 'Deleted place successfully.'})
+  const placeId = req.params.pid;
+  if (!DUMMY_PLACES.find(p => p.id === placeId)) {
+    throw new HttpError('Could not find a place for that id.', 404);
+  }
+  DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
+  res.status(200).json({ message: 'Deleted place.' });
 };
 
 exports.getPlaceById = getPlaceById;
