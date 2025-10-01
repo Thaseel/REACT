d8cb7bea2c06ec1d4ce87751e4f6a077ef73b0aa
Thaseel <thaseelahmed555@gmail.com>
2025-10-01 23:39:14 +0530

    Creating & storing Documents in the database - 128

--- BACKEND/controllers/places-controller.js
+++ BACKEND/controllers/places-controller.js
@@ -3,6 +3,7 @@ const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
 const getCoordsForAddress = require('../util/location');
+const Place = require('../models/place');
 
 let DUMMY_PLACES = [
   {
@@ -53,9 +54,9 @@ const getPlacesByUserId = (req, res, next) => {
 
 const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
-//   if (!errors.isEmpty()) {
-//     return next(new HttpError('Invalid inputs passed, please check your data.', 422));
-//   }
+  if (!errors.isEmpty()) {
+    return next(new HttpError('Invalid inputs passed, please check your data.', 422));
+  }
 
   const { title, description, address, creator } = req.body;
 
@@ -67,16 +68,20 @@ const createPlace = async (req, res, next) => {
   }
 
   // const title = req.body.title;
-  const createdPlace = {
-    id: uuidv4(),
+  const createdPlace = new Place({
     title,
     description,
-    location: coordinates,
     address,
+    image : 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/400px-Empire_State_Building_%28aerial_view%29.jpg',
     creator
-  };
+  });
 
-  DUMMY_PLACES.push(createdPlace); //unshift(createdPlace)
+  try{
+    await createdPlace.save();
+  } catch (err) {
+    const error = new HttpError('Creating Place failed, please try again.', 500);
+    return next(error);
+  }
 
   res.status(201).json({ place: createdPlace });
 };
