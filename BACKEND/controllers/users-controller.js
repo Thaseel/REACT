5955958acd2189e49dd86a2405001346e9eaf7d3
Thaseel <thaseelahmed555@gmail.com>
2025-10-02 19:38:39 +0530

    Getting Users - 137

--- BACKEND/controllers/users-controller.js
+++ BACKEND/controllers/users-controller.js
@@ -14,8 +14,16 @@ const DUMMY_USERS = [
     }
 ];
 
-const getUsers = (req, res, next) => {
-    res.status(200).json({ users: DUMMY_USERS });
+const getUsers = async (req, res, next) => {
+    let users;
+    try{
+        users = await User.find({}, '-password');             // This will fetch all the key value pairs except the password
+    } catch(err){
+        const error = new HttpError('error fetching all the users, please try again later', 500);
+        return next(error);
+    }
+
+    res.json({users : users.map(user => user.toObject({getters : true}))});
 };
 
 const signup = async (req, res, next) => {
